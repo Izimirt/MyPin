ff8cf15a8d9dc7c2708360b99c1558b787ce37cb
Izimirt <andreu77705@gmail.com>
2025-12-20 00:24:44 +0300

    1.0.0

---
+++ include/MyPin.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <Arduino.h>
+
+class MyPin
+{
+    public:
+    
+        MyPin(uint16_t pin,
+              bool isOutput,
+              bool isINPUT_PULLUP = false);
+
+        MyPin();
+
+        void SetPin(uint16_t pin,
+                    bool isOutput,
+                    bool isINPUT_PULLUP = false);
+
+        void Change();
+
+        void Change(bool isOn);
+
+        void AnalogWrite(uint8_t val);
+
+        void StartBlink(uint32_t changeTimeMls);
+
+        void StopBlink(bool state);
+
+        void BlinkHandler();
+
+        bool Read();
+
+        int16_t AnalogRead();
+
+        uint8_t AntiRattleButton(bool signal, uint32_t shortTime = 50, uint32_t longTime = 0);
+
+        uint32_t AntiRattleSensor(bool signal);
+
+        uint8_t GetPin();
+
+        bool GetState();
+
+    private:
+
+        uint16_t pin;
+        bool state;
+        bool isOutput;
+
+        bool blinkFlag = false;
+        uint32_t startMillisBlink;
+        uint32_t changeTimeMls;
+
+        bool previousReaderBtn;
+        bool ready = false;
+        uint8_t buttonState = 0;
+        uint32_t startMillisBtn;
+
+        bool previousReaderSns;
+        uint32_t startMillisSns;
+};
---
+++ src/MyPin.cpp
@@ -0,0 +1,161 @@
+#include "MyPin.h"
+
+MyPin::MyPin(uint16_t pin, bool isOutput, bool isINPUT_PULLUP)
+{
+    SetPin(pin,isOutput,isINPUT_PULLUP);
+}
+
+MyPin::MyPin()
+{ 
+}
+
+void MyPin::SetPin(uint16_t pin, bool isOutput, bool isINPUT_PULLUP)
+{
+    this-> pin = pin;
+    this-> isOutput = isOutput;
+    state = false;
+
+    if(isOutput) 
+    {
+        pinMode(pin, OUTPUT);
+        digitalWrite(pin, false);
+    }
+    else if(isINPUT_PULLUP)
+        pinMode(pin, INPUT_PULLUP);
+    else
+        pinMode(pin, INPUT);
+}
+
+void MyPin::Change()
+{
+    Change(!state);
+}
+
+void MyPin::Change(bool isOn)
+{
+    digitalWrite(pin, isOn);
+    state = isOn;
+}
+
+void MyPin::AnalogWrite(uint8_t val)
+{
+    analogWrite(pin,val);
+}
+
+void MyPin::StartBlink(uint32_t changeTimeMls)
+{
+    if(!blinkFlag)
+    {
+        blinkFlag = true;
+        startMillisBlink = millis();
+    }
+    this->changeTimeMls = changeTimeMls;
+}
+
+void MyPin::StopBlink(bool state)
+{
+    if(blinkFlag)
+    {
+        blinkFlag = false;
+        digitalWrite(pin, state);
+    }
+    
+}
+
+void MyPin::BlinkHandler()
+{
+    if(blinkFlag)
+    {
+        uint32_t mls = millis();
+        if(mls - startMillisBlink >= changeTimeMls)
+        {
+            Change();
+            startMillisBlink = mls;
+        } 
+    }
+}
+
+bool MyPin::Read()
+{
+    return digitalRead(pin);
+}
+
+int16_t MyPin::AnalogRead()
+{
+    return analogRead(pin);
+}
+
+uint8_t MyPin::AntiRattleButton(bool signal, uint32_t shortTime, uint32_t longTime) 
+{
+    bool reader = digitalRead(pin);
+    if(!signal)
+        reader = !reader;
+
+    if(reader)
+    {
+        if(ready)
+            return 0;
+
+        uint32_t mls = millis();
+        if(!previousReaderBtn)
+            startMillisBtn = mls;
+        
+        if(mls - startMillisBtn > shortTime)
+        {
+            buttonState = 1;
+
+            if(longTime > shortTime)
+            {
+                if(mls - startMillisBtn > longTime)
+                {
+                    buttonState = 2;
+                    ready = true;
+                }
+            }
+            else
+                ready = true;
+        }
+    }
+    else
+        ready = false;
+
+    previousReaderBtn = reader;
+
+    if(ready || (!ready && buttonState))
+    {
+        uint8_t bS = buttonState;
+        buttonState = 0;
+        return bS;
+    }
+
+    return 0;
+}
+
+uint32_t MyPin::AntiRattleSensor(bool signal)
+{
+    bool reader = digitalRead(pin);
+    if(!signal)
+        reader = !reader;
+
+    uint32_t result = 0;
+    if(reader)
+    {
+        uint32_t mls = millis();
+        if(!previousReaderSns)
+            startMillisSns = mls;
+
+        result = mls - startMillisSns;
+    }
+    previousReaderSns = reader;
+    return result;
+}
+
+uint8_t MyPin::GetPin()
+{
+    return pin;
+}
+
+bool MyPin::GetState()
+{
+    return state;
+}
